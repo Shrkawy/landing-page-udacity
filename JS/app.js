21ee653e6040bee3710507c5070c36e28009b0b5
Sharkawy <mahmoud.alshrkawy@gmail.com>
2021-01-12 15:53:49 +0200

    active class nav

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,28 @@
+// select elements
+const sections = document.querySelectorAll('section');
+const nav = document.querySelector('#nav-list');
+const header = document.querySelector('header');
+
+
+
+// Build navbar
+for (const section of sections) {
+  let navItem = `
+    <li>
+      <a id="${section.id + "-nav"}" class href="${section.id}">${section.dataset.nav}</a>
+    </li>`;
+  nav.insertAdjacentHTML('beforeend', navItem);
+}
+
+// add active class to Section 1
+
+document.addEventListener('readystatechange', () => {
+  const liActive = document.querySelector('#section-1-nav');
+  liActive.classList.add('active')
+})
+
+
+
 // smooth scrolling
 
 const links = document.querySelectorAll("#header ul a");
@@ -11,7 +36,7 @@ function clickHandler(link) {
 
   const href = this.getAttribute("href");
 
-  document.querySelector(href).scrollIntoView({
+  document.getElementById(href).scrollIntoView({
     behavior: "smooth"
   })
 }
@@ -19,11 +44,9 @@ function clickHandler(link) {
 // hide scrollBar when scroll down
 
 let prevScrollpos = window.pageYOffset;
-const header = document.getElementById('header');
 
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
-
   if (prevScrollpos > currentScrollPos) {
     header.style.top = "0";
   } else {
@@ -34,35 +57,18 @@ window.onscroll = () => {
 
 // avtive class Navbar
 
-let scrollPos = window.scrollY;
-let liActive = document.querySelector('.active');
-
-window.addEventListener('scroll', toggleActive);
-
-function toggleActive() {
-  scrollPos = window.scrollY;
-  if (scrollPos < 550) {
-    liActive.classList.remove('active')
-    let newLiActive = document.querySelector('#company')
-    newLiActive.classList.add('active')
-    liActive = newLiActive;
-
-  } else if (scrollPos >= 550 && scrollPos < 1070 ) {
-    liActive.classList.remove('active')
-    let newLiActive = document.querySelector('#features')
-    newLiActive.classList.add('active')
-    liActive = newLiActive
-
-  } else if (scrollPos >= 1070 && scrollPos < 1710){
-    liActive.classList.remove('active')
-    let newLiActive = document.querySelector('#pricing')
-    newLiActive.classList.add('active')
-    liActive = newLiActive
-
-  } else {
-    liActive.classList.remove('active')
-    let newLiActive = document.querySelector('#subscribe')
-    newLiActive.classList.add('active')
-    liActive = newLiActive
-  }
+onScroll = () => {
+  const sections = [...document.querySelectorAll('#header ul a')];
+  const scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  sections.forEach((currLink) => {
+    const val = currLink.getAttribute('href');
+    const refElement = document.getElementById(val);
+    if (refElement.offsetTop <= scrollPos && (refElement.offsetTop + refElement.offsetHeight > scrollPos)) {
+      currLink.classList.add('active');
+    } else {
+      currLink.classList.remove('active');
+    }
+  });
 }
+
+document.addEventListener('scroll', onScroll);
